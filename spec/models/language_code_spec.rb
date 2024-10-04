f78b355d8322a7f879d3adaaa4ad285190d1e95f
Robert-Anthony Lee-Faison <108823963+leefaisonr@users.noreply.github.com>
2024-10-04 16:31:16 -0400

    Start extracting languages (#17)
    * WIP: Start extracting languages
    
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Sean Warren <seanwarren77@users.noreply.github.com>
    
    * Generate model for languages
    
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Denzil Phillips <dphillips-39@users.noreply.github.com>
    Co-authored-by: Sean Warren <seanwarren77@users.noreply.github.com>
    
    * added class method to language code to find config file
    
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * delete stray file
    
    * load languages from file
    
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Sean Warren <seanwarren77@users.noreply.github.com>
    Co-authored-by: Denzil Phillips <dphillips-39@users.noreply.github.com>
    Co-authored-by: Sean Warren <seanwarren77@gmail.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

---
+++ spec/models/language_code_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe LanguageCode, type: :model do
+  let(:language_code) {LanguageCode.new(code: "por", language_name: "Portuguese")}
+
+  it "can instantiate" do
+    expect(language_code).to be_instance_of(LanguageCode)
+  end
+
+  it "returns a code" do
+    expect(language_code.language_name).to eq "Portuguese"
+    expect(language_code.code).to eq "por"
+  end
+
+  context 'when loading all of the language codes' do
+    it "knows where the language codes are" do
+      expect(LanguageCode.config_file).to eq Rails.root.join("config","language_codes","iso-639-3.tab").to_s
+    end
+
+    it "loads all of the language codes" do
+      expect(LanguageCode.count).to eq 0
+      LanguageCode.load_languages 
+      expect(LanguageCode.count).to eq 7920
+    end
+  end
+end
